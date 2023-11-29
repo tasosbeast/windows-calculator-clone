0f14205059f0f6c7c0278c0137fe320c0e898a3c
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2023-11-29 21:52:24 +0200

    Refactor calculator button event listener and add operate function

--- script.js
+++ script.js
@@ -4,30 +4,54 @@ let operator;
 let numbersEl = Array.from(document.querySelectorAll(".calc-numbers"));
 console.log(numbersEl);
 let screenEl = document.querySelector(".screen");
-
+let screenValue = "";
+let equals = document.querySelector(".equals");
 numbersEl.forEach((number) => {
-  number.addEventListener(
-    "click",
-    () => (screenEl.textContent += number.textContent)
-  );
+  number.addEventListener("click", () => {
+    screenEl.textContent += number.textContent;
+    screenValue += number.textContent;
+  });
 });
 
-function add(a, b) {
-  return a + b;
+// equals.addEventListener("click", operate());
+
+function add(arr) {
+  return arr.reduce((acc, current) => acc + current);
 }
 
-function subtract(a, b) {
-  return a - b;
+function subtract(arr) {
+  return arr.reduce((acc, current) => acc - current);
 }
 
-function multiply(a, b) {
-  return a * b;
+function multiply(arr) {
+  return arr.reduce((acc, current) => acc * current);
 }
 
-function divide(a, b) {
-  return a / b;
+function divide(arr) {
+  return arr.reduce((acc, current) => acc / current);
 }
 
-function operate(operator, firstNumber, secondNumber) {
-  return operator(firstNumber, secondNumber);
+console.log(add([1, 2, 3, 4, 5])); // Should print 15
+console.log(subtract([20, 5, 3])); // Should print 12
+console.log(multiply([2, 3, 4])); // Should print 24
+console.log(divide([100, 5, 2])); // Should print 10
+
+function operate(firstNum, operatorr, secondNum) {
+  const nums = [firstNum, secondNum];
+  if (operatorr === "+") {
+    return add(nums);
+  } else if (operatorr === "-") {
+    return subtract(nums);
+  } else if (operatorr === "*") {
+    return multiply(nums);
+  } else if (operatorr === "/") {
+    return divide(nums);
+  }
 }
+
+console.log(operate(4, "+", 2));
+//firstNumber = firstNum;
+//   secondNumber = secondNum;
+
+//   return operatorr(firstNum, secondNum);
+// }
