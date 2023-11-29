27df6542c4a08dd32e99e966d19fd15baa1dbc65
tasosbeast <66928134+tasosbeast@users.noreply.github.com>
2023-11-29 20:00:22 +0200

    Update calculator display and add event listeners for numbers

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let firstNumber;
+let secondNumber;
+let operator;
+let numbersEl = Array.from(document.querySelectorAll(".calc-numbers"));
+console.log(numbersEl);
+let screenEl = document.querySelector(".screen");
+
+numbersEl.forEach((number) => {
+  number.addEventListener("click");
+});
+
 function add(a, b) {
   return a + b;
 }
@@ -12,4 +23,8 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+  return operator(firstNumber, secondNumber);
 }
